1e10a31d82c750dbee52a0235a3bf49cb2816fc9
Nathan <nathan@musicnate.ca>
2016-07-14 22:51:19 -0400

    Change airline to lightline and add patched fonts to system.

--- vim_setup.sh
+++ vim_setup.sh
@@ -16,11 +16,16 @@ if [ ! -d ~/.vim/autoloads/ ]; then
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
     
     # GET ALL THE PLUGINS!
-    git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/easymotion
     git clone git://github.com/tpope/vim-surround ~/.vim/bundle/surround
     git clone git://github.com/tpope/vim-commentary.git ~/.vim/bundle/commentary
-    git clone https://github.com/itchyny/calendar.vim ~/.vim/bundle/calendar
+    git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/easymotion
     git clone https://github.com/kien/rainbow_parenthesis.vim ~/.vim/bundle/rainbow
+    git clone https://github.com/itchyny/calendar.vim ~/.vim/bundle/calendar
+    git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline
+
+    # Get patchet fonts
+    git clone https://github.com/powerline/fonts ~/Documents/fonts
+    bash ~/Documents/fonts/install.sh
 fi
 
 cp ~/Documents/dotfiles/vim/.vimrc ~/
