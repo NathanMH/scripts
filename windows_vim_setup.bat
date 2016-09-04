b24ce25e60bae3a064d5306155a472285c1f9721
Nathan Mador-House <nathan@musicnate.ca>
2016-09-04 18:33:15 -0400

    Add bat file for vim initialization on windows.

---
+++ windows_vim_setup.bat
@@ -0,0 +1,10 @@
+mkdir C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload
+
+call git clone git://github.com/tpope/vim-surround C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
+call git clone git://github.com/tpope/vim-commentary.git C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
+call git clone https://github.com/easymotion/vim-easymotion C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
+call git clone https://github.com/kien/rainbow_parentheses.vim C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
+call git clone https://github.com/itchyny/calendar.vim C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
+call git clone https://github.com/itchyny/lightline.vim C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
+call git clone https://github.com/Yggdroot/indentLine C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
+call git clone https://github.com/Raimondi/delimitMate.git C:\Users\Nathan Mador-House\Documents\gVimPortable\Data\settings\vimfiles\autoload\
