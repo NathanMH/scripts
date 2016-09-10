87061fd2615a202089a6840ce8471a677eb88081
Nathan <nathan@musicnate.ca>
2016-09-10 11:06:36 -0700

    Add JRE to linux setup.

--- linux_package_installer.sh
+++ linux_package_installer.sh
@@ -2,12 +2,12 @@
 
 apt-get update
 apt-get install cmus -y # Music player
-apt-get install emacs -y # Epub reader
-apt-get install fbreader -y
+apt-get install emacs -y # The other vim
 apt-get install filezilla -y # Alternative?
-apt-get install git-all -y # Git all the things!
+apt-get install git-all -y # Git
 apt-get install i3wm -y # Does it come with i3status/lock?
 apt-get install mpv -y # Video player
+apt-get install default-jre # Java runtime environment
 apt-get install cmake
 
 
@@ -25,7 +25,7 @@ apt-get install xboxdrv -y # Controller support
 
 # Languages
 apt-get install lua5.2 -y # Get 5.3 if available
-apt-get install python3.4 -y
+apt-get install python3.5 -y
 apt-get install ruby2.1 -y # Get 2.3 if available
 
 
