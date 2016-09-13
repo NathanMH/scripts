102267acafeb66b417e852f2e04a345b6d77504e
Nathan <nathan@musicnate.ca>
2016-09-12 20:28:04 -0700

    Refactor web scraping. OOP coming.

--- bc_bounty_check.py
+++ bc_bounty_check.py
@@ -1,49 +1,94 @@
-#!/bin/sh
+"""####################
+Author: Nathan Mador-House
+####################"""
 
-# Searches for new open issues from BC open government pay for pull program.
+#######################
+"""####################
+Index:
+    1. Imports and Readme
+    2. Functions
+    3. Main
+    4. Testing
+####################"""
+#######################
+
+###################################################################
+# 1. IMPORTS AND README
+###################################################################
 
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
-url = "https://github.com/search?utf8=%E2%9C%93&q=org%3Abcgov+%241000&type=Issues&ref=searchresults"
+# Searches for new open issues from BC open government pay for pull program.
+
+###################################################################
+# 2. FUNCTIONS
+###################################################################
+
+BOUNTIES_URL = "https://github.com/search?utf8=%E2%9C%93&q=org%3Abcgov+%241000&type=Issues&ref=searchresults"
 
 
 def make_html(url):
+    """ Make soup object """
+
     html = urllib.request.Request(url)
     response = urllib.request.urlopen(html)
-    return response.read().decode('utf-8')
+    html_response = response.read().decode('utf-8')
+    the_soup = BeautifulSoup(html_response, 'html.parser')
+    return the_soup
 
 
-def make_soup(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
+def previous_bounties(bounties_list):
+    """ Get list of previous bounties """
+
+    previous_bounties = []
+    with open(bounties_list, 'r') as text_file:
+        previous_bounties = text_file.readlines()
+
+    number_of_previous_bounties = len(previous_bounties)
+    print("Previous Bounties: " + str(number_of_previous_bounties))
+    for i in previous_bounties:
+        print(str(i).strip())
 
 
-def check_number_of_bounties(soup, filename):
-    with open(filename, 'r') as text_file:
-        bounties = text_file.readline()
+def current_bounties(soup):
+    """ Get list of current bounties """
 
-    number_active_bounties = len(soup.find_all("svg", class_="octicon octicon-issue-opened open"))
-    print("Previous Bounties: " + bounties.rstrip())
-    print("Current Bounties: " + str(number_active_bounties))
-    print("---------------")
+    # Active Bounties
+    active_bounties = soup.find_all("svg", class_="octicon octicon-issue-opened open")
+    total_active_bounties = len(active_bounties)
+    print("Active Bounties: " + str(total_active_bounties))
 
-    all_bounties = soup.find_all("p", class_="title")
-    for i in all_bounties:
-        print(i)
-        print()
+    # Bounties listed
+    bounties = soup.find_all(class_="issue-list-item public")
+    total_current_bounties = len(bounties)
+    print("Total bounties: " + str(total_current_bounties))
 
-    # active_bounties = []
-    # for i in range(1, number_active_bounties):
-    #     print(all_bounties[i])
-    #     active_bounties[i] = all_bounties[i]
-    # print(len(active_bounties)ne)
-    # for i in active_bounties:
-    #     print(i)
+    # Bounty links
+    bounty_links = soup.find_all("a", href=re.compile("/bcgov/"))
+    print(bounty_links[0])
+    # Tuple of [Key, Title, Link]
+    tuple 
 
+    
+def save_current_bounties(bounties, bounties_file):
+    for i in bounties:
+        with open(bounties_file, 'w') as text_file:
+            text_file.write(i)
 
-# def __main__(url):
+###################################################################
+# 3. MAIN
+###################################################################
 
-html = make_html(url)
-soup = make_soup(html)
-check_number_of_bounties(soup, 'previous_bounties.txt')
+def main(url):
+
+    soup = make_html(url)
+    # previous_bounties('previous_bounties.txt')
+    current_bounties(soup)
+
+main(BOUNTIES_URL)
+
+###################################################################
+# 4. TESTING
+###################################################################
