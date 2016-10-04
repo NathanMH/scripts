63759c7130bd28a7772ea754926fc0fb602c3f96
Nathan Mador-House <nathan@musicnate.ca>
2016-10-03 22:23:59 -0700

    Trying to fix linux setup.

--- linux_complete_setup.sh
+++ linux_complete_setup.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install emacs -y # The other vim
-apt-get install git-all -y # Git
+apt-get install git -y # Git
 apt-get install i3wm -y # Does it come with i3status/lock?
 apt-get install i3status -y # Status bar+
 apt-get install i3 # Extras?
@@ -29,25 +29,26 @@ apt-get install python3.5 -y
 apt-get install ruby2.1 -y # Get 2.3 if available
 
 # Get necessary config files
-git clone https://github.com/NathanMH/dotfiles ~/Documents/
+git clone https://github.com/NathanMH/dotfiles ~/Documents/dotfiles
 
 # Make symlinks and other minor changes
+rm /etc/lxdm/lxdm.conf
 ln -s ~/Documents/dotfiles/lxdm /etc/lxdm/lxdm.config
+rm ~/.Xmodmap
 ln -s ~/Documents/dotfiles/.Xmodmap ~/.Xmodmap
+rm ~/.bashrc
 ln -s ~/Documents/dotfiles/.bashrc ~/.bashrc
+rm ~/.inputrc
 ln -s ~/Documents/dotfiles/.inputrc ~/.inputrc
+rm ~/.xinitrc
 ln -s ~/Documents/dotfiles/.xinitrc ~/.xinitrc
-
-alias py = python3
-alias pytest = pytest3
 
 # Setup i3
 mkdir ~/.i3/
+rm ~/.i3status.conf
 ln -s ~/Documents/dotfiles/i3/i3status.conf ~/.i3status.conf
 ln -s ~/Documents/dotfiles/i3/i3/ ~/.i3/
 
 # Other
 setxkbmap -layout "us" -variant colemak
 xmodmap ~/.Xmodmap
-~/Documents/scripts/vim_setup.sh
-~/Documents/scripts/emacs_setup.sh
