1bd5f0b2cc3426c8c664211f1d9f332204c3aff2
Nathan Mador-House <nathan@musicnate.ca>
2016-10-16 23:29:49 +0000

    Add keyboard setup to .profile.

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Make symlinks and other minor changes
+mkdir ~/tmp
+
+# General symlinks
 rm /etc/lxdm/lxdm.conf
 ln -s ~/Documents/dotfiles/lxdm /etc/lxdm/lxdm.config
 rm ~/.Xmodmap
@@ -11,13 +13,18 @@ rm ~/.inputrc
 ln -s ~/Documents/dotfiles/.inputrc ~/.inputrc
 rm ~/.xinitrc
 ln -s ~/Documents/dotfiles/.xinitrc ~/.xinitrc
+rm ~/.profile
+ln -s ~/Documents/dotfiles/.profile ~/.profile
 
 # Setup i3
 mkdir ~/.i3/
-rm ~/.i3status.conf
-ln -s ~/Documents/dotfiles/i3/i3status.conf ~/.i3status.conf
-ln -s ~/Documents/dotfiles/i3/i3/ ~/.i3/
+rm ~/.i3blocks.conf
+ln -s ~/Documents/dotfiles/i3/i3blocks.conf ~/.i3status.conf
+# rm ~/.i3status.conf
+# ln -s ~/Documents/dotfiles/i3/i3status.conf ~/.i3status.conf
+rm -r ~/.i3
+ln -s ~/Documents/dotfiles/i3/ ~/.i3
 
 # Other
-setxkbmap -layout "us" -variant colemak
+setxkbmap us colemak
 xmodmap ~/.Xmodmap
