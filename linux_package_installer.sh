f1db1a52fede4eed394d8aa720094cf42e71cf82
Nathan Mador-House <nathan@musicnate.ca>
2016-11-17 19:08:17 -0800

    Update linux package installer.

--- linux_package_installer.sh
+++ linux_package_installer.sh
@@ -1,33 +1,44 @@
 #!/bin/sh
 
 apt-get update
-apt-get install emacs -y # The other vim
 apt-get install git -y # Git
 apt-get install i3wm -y # Does it come with i3status/lock?
 apt-get install i3status -y # Status bar+
-apt-get install i3 # Extras?
-apt-get install mpv -y # Video player
-apt-get install default-jre # Java runtime environment
-apt-get install cmake
-apt-get install xbacklight
+apt-get install i3blocks -y
+apt-get install i3 -y # Extras?
+apt-get install default-jre -y # Java runtime environment
+apt-get install cmake -y
+apt-get install xbacklight -y
 
-# Mu4e plus dependencies (including optional pdf and html viewers)
-apt-get install mu4e -y
-apt-get install libwebkit-dev -y
-apt-get install guile-2.0-dev -y
-apt-get install libgmime-2.6-dev -y
-apt-get install libxapian-dev -y
-apt-get install offlineimap -y
+
+# Other Applications
+apt-get install cmus -y # Music Player
+apt-get install emacs -y # The other vim
+apt-get install firefox -y # Better browsing
+apt-get install mpv -y # Video player
+apt-get install mutt -y # Email
+apt-get install rxvt-unicode -y # Terminal
+apt-get install vim -y # One true Editor
+apt-get install vim-gnome-py2 -y # For python 2 compatibility (for latex)
+apt-get install weechat -y # IRC/Slack chat
+apt-get install zeal -y # Offline programming documentatio
+apt-get install zsh -y # Shell
 
 # Other
+apt-get install feh -y # Background image manager
+apt-get install offlineimap -y # Offline emails
+apt-get install powertop -y # Power stats
+apt-get install tlp -y # Power management
 apt-get install usbmount -y # For auto usb mounting
 apt-get install vim-gnome -y # For copy/paste support
+apt-get install youtube-dl -y # Youtube downloader
 apt-get install xboxdrv -y # Controller support
 
 # Programming Languages
 apt-get install lua5.2 -y # Get 5.3 if available
 apt-get install python3.5 -y
+apt-get install python-pip -y
 apt-get install ruby2.3 -y # Get 2.3 if available
-
-# TO ADD
-minitube
+apt-get install luarocks -y
+apt-get install texlive
+apt-get install texlive-latex-extra
