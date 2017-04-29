b982b351c38b75e15c205a87de5d2e42960c3732
NathanMH <nathan@musicnate.ca>
2017-04-28 19:47:06 -0700

    Add vim 8 plugin script.

---
+++ vim-8-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/tpope/vim-surround ~/.vim/pack/musicnate/start/surround
+git clone https://github.com/tpope/vim-commentary ~/.vim/pack/musicnate/start/commentary
+git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/musicnate/start/lightline
+git clone https://github.com/itchyny/calendar.vim ~/.vim/pack/musicnate/start/calendar
+git clone https://github.com/Raimondi/delimitMate.git ~/.vim/pack/musicnate/start/delimitMate
+git clone https://github.com/Yggdroot/indentLine ~/.vim/pack/musicnate/start/indentLine
+git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/musicnate/start/easymotion
+git clone https://github.com/kien/rainbow_parentheses.vim ~/.vim/pack/musicnate/start/rainbowparentheses
+git clone https://github.com/xuhdev/vim-latex-live-preview ~/.vim/pack/musicnate/start/latexlivepreview
+git clone https://github.com/mhinz/vim-startify ~/.vim/pack/musicnate/start/startify
+git clone https://github.com/vimwiki/vimwiki ~/.vim/pack/musicnate/start/vimwiki
