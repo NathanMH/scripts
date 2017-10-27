18053995eda35ebce1743520dae1c628578ab8b6
NathanMH <nathan@musicnate.ca>
2017-10-27 13:54:06 -0400

    Fix output argument and add rewrite protection.

--- emacs-extra.sh
+++ emacs-extra.sh
@@ -1,9 +1,13 @@
 # Should this just be git clone emacs-calfw?
-wget https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-ical.el -o /home/musicnate/.emacs.d/elpa/calfw-ical.el
-wget https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-org.el -o /home/musicnate/.emacs.d/elpa/calf-org.el
-wget https://raw.githubusercontent.com/jonathanchu/emacs/master/vendor/hl-tags-mode.el -o /home/musicnate/.emacs.d/elpa/hl-tags-mode.el
-wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/latex-preview-pane.el -o /home/musicnate/.emacs.d/elpa/latex-preview-pane.el
-wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-latex-preview-pane-welcome.txt -o /home/musicnate/.emacs.d/elpa/message-latex-preview-pane-welcome.txt
-wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-no-preview-yet.txt -o /home/musicnate/.emacs.d/elpa/message-no-preview-yet.txt
-wget https://raw.githubusercontent.com/jepugs/emacs-soft-tab/master/soft-tab.el -o /home/musicnate/.emacs.d/elpa/soft-tab.el
+
+wget -nc https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-ical.el -O /home/musicnate/.emacs.d/elpa/calfw-ical.el
+wget -nc https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-org.el -O /home/musicnate/.emacs.d/elpa/calfw-org.el
+
+wget -nc https://raw.githubusercontent.com/jonathanchu/emacs/master/vendor/hl-tags-mode.el -O /home/musicnate/.emacs.d/elpa/hl-tags-mode.el
+
+wget -nc https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/latex-preview-pane.el -O /home/musicnate/.emacs.d/elpa/latex-preview-pane.el
+wget -nc https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-latex-preview-pane-welcome.txt -O /home/musicnate/.emacs.d/elpa/message-latex-preview-pane-welcome.txt
+wget -nc https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-no-preview-yet.txt -O /home/musicnate/.emacs.d/elpa/message-no-preview-yet.txt
+
+wget -nc https://raw.githubusercontent.com/jepugs/emacs-soft-tab/master/soft-tab.el -O /home/musicnate/.emacs.d/elpa/soft-tab.el
 
