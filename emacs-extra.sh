1cced8a51c8d4a91440a6a3922a0a6d1a7bc3588
NathanMH <nathan@musicnate.ca>
2017-10-27 13:14:12 -0400

    Add destination path to emacs extras script.

--- emacs-extra.sh
+++ emacs-extra.sh
@@ -1,12 +1,9 @@
 # Should this just be git clone emacs-calfw?
-wget https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-ical.el
-wget https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-org.el
+wget https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-ical.el -o /home/musicnate/.emacs.d/elpa/calfw-ical.el
+wget https://raw.githubusercontent.com/kiwanami/emacs-calfw/master/calfw-org.el -o /home/musicnate/.emacs.d/elpa/calf-org.el
+wget https://raw.githubusercontent.com/jonathanchu/emacs/master/vendor/hl-tags-mode.el -o /home/musicnate/.emacs.d/elpa/hl-tags-mode.el
+wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/latex-preview-pane.el -o /home/musicnate/.emacs.d/elpa/latex-preview-pane.el
+wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-latex-preview-pane-welcome.txt -o /home/musicnate/.emacs.d/elpa/message-latex-preview-pane-welcome.txt
+wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-no-preview-yet.txt -o /home/musicnate/.emacs.d/elpa/message-no-preview-yet.txt
+wget https://raw.githubusercontent.com/jepugs/emacs-soft-tab/master/soft-tab.el -o /home/musicnate/.emacs.d/elpa/soft-tab.el
 
-
-wget https://raw.githubusercontent.com/jonathanchu/emacs/master/vendor/hl-tags-mode.el
-
-wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/latex-preview-pane.el
-wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-latex-preview-pane-welcome.txt
-wget https://raw.githubusercontent.com/jsinglet/latex-preview-pane/master/message-no-preview-yet.txt
-
-wget https://raw.githubusercontent.com/jepugs/emacs-soft-tab/master/soft-tab.el
