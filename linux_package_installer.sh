e07dc75d56a53b703c83694abe7842218be318ed
NathanMH <nathan@musicnate.ca>
2020-08-02 13:59:47 -0400

    remove bad scripts.

--- linux_package_installer.sh
+++ linux_package_installer.sh
@@ -16,21 +16,15 @@ apt-get install cmus -y # Music Player
 apt-get install emacs -y # The other vim
 apt-get install firefox -y # Better browsing
 apt-get install mpv -y # Video player
-apt-get install mutt -y # Email
-apt-get install rxvt-unicode -y # Terminal
 apt-get install vim -y # One true Editor
-apt-get install vim-gnome-py2 -y # For python 2 compatibility (for latex)
 apt-get install weechat -y # IRC/Slack chat
 apt-get install zeal -y # Offline programming documentatio
-apt-get install zsh -y # Shell
 
 # Other
 apt-get install feh -y # Background image manager
-apt-get install offlineimap -y # Offline emails
 apt-get install powertop -y # Power stats
 apt-get install tlp -y # Power management
 apt-get install usbmount -y # For auto usb mounting
-apt-get install vim-gnome -y # For copy/paste support
 apt-get install youtube-dl -y # Youtube downloader
 apt-get install xboxdrv -y # Controller support
 
@@ -39,6 +33,3 @@ apt-get install lua5.2 -y # Get 5.3 if available
 apt-get install python3.5 -y
 apt-get install python-pip -y
 apt-get install ruby2.3 -y # Get 2.3 if available
-apt-get install luarocks -y
-apt-get install texlive
-apt-get install texlive-latex-extra
--- vim-8-setup.sh
+++ vim-8-setup.sh
@@ -16,9 +16,7 @@ git clone https://github.com/Raimondi/delimitMate.git ~/.vim/pack/musicnate/star
 git clone https://github.com/Yggdroot/indentLine ~/.vim/pack/musicnate/start/indentLine
 git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/musicnate/start/easymotion
 git clone https://github.com/kien/rainbow_parentheses.vim ~/.vim/pack/musicnate/start/rainbowparentheses
-git clone https://github.com/xuhdev/vim-latex-live-preview ~/.vim/pack/musicnate/start/latexlivepreview
 git clone https://github.com/mhinz/vim-startify ~/.vim/pack/musicnate/start/startify
-git clone https://github.com/vimwiki/vimwiki ~/.vim/pack/musicnate/start/vimwiki
 
 # Get fonts
 git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git ~/.fonts/adobe-fonts/source-code-pro
