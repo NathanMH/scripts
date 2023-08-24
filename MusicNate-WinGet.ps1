07dba658a5051b42956147c80c523ea5199f9d6d
NathanMH <nathan@musicnate.ca>
2023-08-24 12:04:46 -0400

    Add regkey script for Win+L

---
+++ MusicNate-WinGet.ps1
@@ -0,0 +1,71 @@
+mkdir "$HOME\Desktop\GIT REPOS"
+git clone https://github.com/NathanMH/scripts.git "$HOME\Desktop\GIT REPOS\"
+
+# General Purpose
+winget install --id=Flow-Launcher.Flow-Launcher -e  ;           # FlowLauncher
+winget install --id=Obsidian.Obsidian -e  ;                     # Obsidian
+winget install --id=Mozilla.Firefox -e  ;                       # Firefox
+winget install --id=Google.Chrome -e  ;                         # Chrome
+winget install --id=Mozilla.Thunderbird -e  ;                   # Thunderbird
+winget install --id=Anki.Anki -e  ;                             # Anki
+winget install --id=voidtools.Everything -e  ;                  # Everything
+winget install --id=TheDocumentFoundation.LibreOffice -e  ;     # LibreOffice
+winget install --id=Microsoft.PowerToys -e  ;                   # PowerToys
+winget install --id=DominikReichl.KeePass -e  ;                 # KeePass
+winget install --id=kvakulo.switcheroo -e  ;                    # Switcheroo
+winget install --id SumatraPDF.SumatraPDF -e ;                  # SumatraPDF
+
+# Media
+winget install --id=VideoLAN.VLC -e  ;                          # VLC
+winget install --id=PrestonN.FreeTube -e  ;                     # FreeTube
+winget install --id=HandBrake.HandBrake -e  ;                   # Handbrake
+winget install --id=ImageMagick.ImageMagick -e  ;               # ImageMagick
+winget install --id=Avidemux.Avidemux -e  ;                     # Avidemux (simple video editor)
+winget install --id=yt-dlp.yt-dlp  -e ;                         # YouTube downloader
+winget install --id=Streamlink.Streamlink -e  ;                 # Twitch stream link
+winget install --id=Streamlink.Streamlink.TwitchGui -e  ;       # Twitch GUI
+winget install --id IrfanSkiljan.IrfanView -e ;                 # IrfanView
+
+# Dev Work
+winget install --id=Microsoft.VisualStudioCode -e  ;            # Visual Studio Code
+winget install --id=vim.vim -e  ;                               # Vim
+winget install --id=Notepad++.Notepad++ -e  ;                   # Notepad++
+winget install --id=Git.Git -e  ;                               # Git
+winget install --id=OlegShparber.Zeal -e  ;                     # Zeal
+winget install --id=PuTTY.PuTTY -e  ;                           # PuTTY
+winget install --id=ProjectJupyter.JupyterLab -e  ;             # Jupyter Notebook
+winget install --id=Postman.Postman -e  ;                       # Postman API
+winget install --id=DBBrowserForSQLite.DBBrowserForSQLite -e  ; # SQLite Browser
+winget install --id=Microsoft.PowerBI  -e ;                     # PowerBI
+
+# Specific Tasks
+winget install --id=Prusa3D.PrusaSlicer -e  ;                   # Prusa Slicer
+winget install --id=7zip.7zip -e  ;                             # 7-Zip
+winget install --id=GIMP.GIMP -e  ;                             # GIMP
+winget install --id=OBSProject.OBSStudio -e  ;                  # OBS
+winget install --id=AntibodySoftware.WizTree -e  ;              # WizTree
+winget install --id=code52.Carnac -e  ;                         # Carnac
+winget install --id=AutoHotkey.AutoHotkey -e  ;                 # AutoHotKey
+winget install --id=qBittorrent.qBittorrent -e                  # qBittorrent
+
+# Utility / Driver
+winget install --id Logitech.OptionsPlus -e ;                   # Logitech Options
+winget install --id=Google.Drive -e  ;                          # Google Drive
+winget install --id=BartoszCichecki.LenovoLegionToolkit  -e ;   # Lenovo Legion
+
+# Games / Fun
+winget install --id=Valve.Steam -e  ;                           # Steam
+winget install --id=EpicGames.EpicGamesLauncher -e  ;           # Epic Games Store
+winget install --id=Ubisoft.Connect -e  ;                       # Ubisoft Connect
+winget install --id=ItchIo.Itch -e  ;                           # Itch.io
+winget install --id=VirtualDesktop.Streamer -e  ;               # Virtual Desktop Streamer
+winget install --id=rocksdanister.LivelyWallpaper -e  ;         # Lively Wallpaper
+
+
+# FlowLauncher - needs GIT REPOS cloned and Version number changed.
+#mklink /H "C:\Users\natha\Desktop\PortableApps\FlowLauncher\app-1.9.5\UserData\Settings\Favorites.conf" "C:\Users\natha\Desktop\ObsidianNotes\Misc. Assets\Windows Setup Misc Options\Favorites.conf"
+#mklink /J "C:\Users\natha\Desktop\PortableApps\FlowLauncher\app-1.9.5\UserData\Plugins\Shortcuts" "C:\Users\natha\Desktop\GIT REPOS\scripts\FlowLauncherShortcutsPlugin"
+
+
+
+
